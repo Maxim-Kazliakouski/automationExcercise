6a00f8b43723e3534f8fc94206fd83c6eb274895
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-06-17 18:06:26 +0300

    feat(): adding dto Account

---
+++ src/test/java/dto/Account.java
@@ -0,0 +1,33 @@
+package dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Data
+@Builder
+public class Account {
+    // account information
+    public String title;
+    public String name;
+    public String email;
+    public String password;
+    public String dayOfBirth;
+    public String monthOfBirth;
+    public String yearOfBirth;
+    public Object checkBox1;
+    public Object checkBox2;
+    // address information
+    public String firstName;
+    public String lastName;
+    public String company;
+    public String addressRequired;
+    public String address2;
+    public String country;
+    public String state;
+    public String city;
+    public String zipCode;
+    public String mobileNumber;
+
+
+
+}
