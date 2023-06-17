46718635dacac182377c2461058c55925b1b47ae
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-06-17 18:14:45 +0300

    feat(): adding constants for Main page

---
+++ src/test/java/constants/MainPageConstants.java
@@ -0,0 +1,24 @@
+package constants;
+
+public class MainPageConstants {
+    // Locators for buttons in app header
+    public static final String HOME_BUTTON = "//li/a[text()=' Home']";
+//    public static final String PRODUCTS_BUTTON = "//li/a[text()=' Products']";
+    public static final String CART_BUTTON = "//li/a[text()=' Cart']";
+    public static final String SIGNUP_LOGIN_BUTTON = "//li/a[text()=' Signup / Login']";
+    public static final String TEST_CASES_BUTTON = "//li/a[text()=' Test Cases']";
+    public static final String API_TESTING_BUTTON = "//li/a[text()=' API Testing']";
+    public static final String CONTACT_US_BUTTON = "//li/a[text()=' Contact us']";
+
+    // markers to recognize that certain page is opened
+    public static final String PRODUCTS_PAGE_MARKER = "//h2[text()='Category']";
+    public static final String CART_PAGE_MARKER = "//b[text()='Cart is empty!']";
+    public static final String SIGN_UP_LOGIN_PAGE_MARKER = "//h2[text()='Login to your account']";
+    public static final String TEST_CASES_PAGE_MARKER = "//b[text()='Test Cases']";
+    public static final String API_TESTING_PAGE_MARKER = "//b[text()='APIs List for practice']";
+    public static final String CONTACT_US_PAGE_MARKER = "//h2[text()='Contact ']";
+
+
+
+
+}
