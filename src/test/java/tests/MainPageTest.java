f0bae77425c9f804b435332b607c4b22164a68a7
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-06-17 18:15:58 +0300

    feat(): adding tests for Main page

---
+++ src/test/java/tests/MainPageTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import jdk.jfr.Description;
+import org.testng.annotations.Test;
+import tests.base.BaseTest;
+
+public class MainPageTest extends BaseTest {
+//    @DataProvider
+//    public Object[][] tabsNames() {
+//        return new Object[][] {
+//                {"Products"},
+//                {"Cart"},
+//                {"Signup / Login"},
+//                {"API Testing"},
+//                {"Video Tutorials"},
+//                {"Contact us"}
+//        };
+//    }
+
+    @Test
+    @Description("Test for checking opening main page")
+    public void openMainPage() {
+        mainPageSteps
+                .openPage();
+    }
+
+    @Test()
+    @Description("Test for checking workability all tabs in app header")
+    public void checkAllTabsInHeader() {
+        mainPageSteps
+                .openPage()
+                .clickOnHeaderTabs();
+    }
+}
