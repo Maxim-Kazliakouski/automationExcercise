164fe2cb80591ad58f053662c5f05a1c1f91fb1a
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-07-03 18:40:38 +0300

    feat(): rename wrapper

---
+++ src/test/java/tests/wrappers/TextInputLabel.java
@@ -0,0 +1,16 @@
+package tests.wrappers;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static java.lang.String.format;
+
+public class TextInputLabel {
+    String label;
+
+    public TextInputLabel(String label) {
+        this.label = label;
+    }
+
+    public void write(String value) {
+        $x(format("//label[contains(text(),'%s')]//..//input", label)).setValue(value);
+    }
+}
