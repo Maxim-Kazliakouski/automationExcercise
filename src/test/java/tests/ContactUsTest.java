f688e9891259cd9249e6ff075580e1ff9386db64
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-08-25 12:12:16 +0300

    feat(): update ContactUsTest

--- src/test/java/tests/ContactUsTest.java
+++ src/test/java/tests/ContactUsTest.java
@@ -3,18 +3,16 @@ package tests;
 import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.sleep;
-
 public class ContactUsTest extends BaseTest{
     @Test
     @Description("Test for checking that user can contact with support")
     public void sendEmailForSupport() {
-        contactUsSteps
+        contactUsPageSteps
                 .openPage()
-                .createNewEmailBody("/Volumes/Work/automationExcercise/fileForUploading.txt")
+                .createNewEmailBody(chooseOS())
                 .isSuccessNotificationAppeared()
                 .clickOnHomeButtonAtContactUsPage();
         mainPageSteps
-                .isOpened();
+                .isMainPageOpened();
     }
 }
