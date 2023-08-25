2362d69aa8c88da75289189254ffd6101cff0cdd
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-08-25 12:04:57 +0300

    feat(): create CartPageLocators

---
+++ src/test/java/constants/CartPageLocators.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class CartPageLocators {
+    public static final String LIST_OF_ADDED_PRODUCTS = "//h4//a";
+    public static final String PRODUCT_QUANTITY = "//h4//a[text()='%s']//..//..//..//button";
+    public static final String PROCEED_TO_CHECKOUT_BUTTON = "//a[@class='btn btn-default check_out']";
+    public static final String REGISTER_LOGIN_BUTTON = "//u[text()='Register / Login']//ancestor::a";
+}
