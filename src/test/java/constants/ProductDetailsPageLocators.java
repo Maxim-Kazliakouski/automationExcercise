a51364f0fe5fb448edf1c82bbb6c43188ed41e77
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-08-25 12:18:28 +0300

    feat(): create ProductDetailsPageLocators

---
+++ src/test/java/constants/ProductDetailsPageLocators.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class ProductDetailsPageLocators {
+//    public static final By ADD_TO_CART = By.className("btn btn-default cart");
+    public static final String ADD_TO_CART = "//button[@class='btn btn-default cart']";
+
+}
