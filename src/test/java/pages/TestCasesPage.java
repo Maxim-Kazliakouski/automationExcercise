5d815dc100354fcc967b7139ad4e5e3429916fa4
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-08-25 12:23:17 +0300

    feat(): create TestCasesPage

--- src/test/java/pages/TestCasesPage.java
+++ src/test/java/pages/TestCasesPage.java
@@ -1,4 +1,19 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+import static constants.MainPageLocators.TEST_CASES_PAGE_MARKER;
+
 public class TestCasesPage {
+
+    public TestCasesPage openPage() {
+        open("/test_cases");
+        return this;
+    }
+
+    public void isOpened() {
+        $x(TEST_CASES_PAGE_MARKER).shouldBe(Condition.visible);
+    }
 }
