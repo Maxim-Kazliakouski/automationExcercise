d41fb6590fed7b300b0a0dc1274445730e7cb2d7
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-08-25 12:11:00 +0300

    feat(): update ContactUsPageLocators

--- src/test/java/constants/ContactUsPageLocators.java
+++ src/test/java/constants/ContactUsPageLocators.java
@@ -1,6 +1,6 @@
 package constants;
 
-public class ContactUsLocators {
+public class ContactUsPageLocators {
     public static final String CONTACT_US_SUBMIT_BUTTON = "//input[@data-qa='submit-button']";
     public static final String SUCCESS_NOTIFICATION = "//div[text()='Success! Your details have been submitted successfully.']";
     public static final String HOME_BUTTON = ".btn-success";
