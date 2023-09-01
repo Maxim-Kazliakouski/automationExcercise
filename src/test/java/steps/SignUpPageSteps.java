d7e74e548089449f0edc90592ea57cab430296c6
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-01 15:52:22 +0300

    feat(): adding logging

--- src/test/java/steps/SignUpPageSteps.java
+++ src/test/java/steps/SignUpPageSteps.java
@@ -1,9 +1,10 @@
 package steps;
 
 import dto.Account;
-import factories.AccountFactory;
+import lombok.extern.log4j.Log4j2;
 import pages.SignUpLoginPage;
 
+@Log4j2
 public class SignUpPageSteps {
     SignUpLoginPage signUpLoginPage;
 
@@ -12,13 +13,21 @@ public class SignUpPageSteps {
     }
 
     public SignUpPageSteps openPage() {
+        log.info("Opening SignUp / Login page...");
         signUpLoginPage
-                .openPage()
+                .openPage();
+        return this;
+    }
+
+    public SignUpPageSteps isSignUpLoginPageOpened() {
+        signUpLoginPage
                 .isPageOpened();
+        log.info("Sign Up / Login page is opened");
         return this;
     }
 
     public SignUpPageSteps createNewUser(Account newAccount) {
+        log.info("Creating new account...");
         signUpLoginPage
                 .createNewAccount(newAccount);
         return this;
@@ -27,23 +36,33 @@ public class SignUpPageSteps {
     public SignUpPageSteps isAccountCreated() {
         signUpLoginPage
                 .checkCreatedAccount();
+        log.info("New account has been created!");
         return this;
     }
 
     public SignUpPageSteps isLoggedAsCreatedAccount() {
         signUpLoginPage
                 .checkLoggedCreatedAccount();
+        log.info("New created account has been logged with new creds!");
         return this;
     }
 
-    public void isAccountDeleted() {
+    public void isLoggedAccount(String username) {
+        signUpLoginPage
+                .checkLoggedAccount(username);
+        log.info("Account has been logged");
+    }
+
+    public void deleteAccount() {
         signUpLoginPage
                 .deleteCratedAccount();
+        log.info("Account has been deleted!");
     }
 
     public SignUpPageSteps signUpWithCreds(String name, String email) {
         signUpLoginPage
                 .signUp(name, email);
+        log.info("User has been signed up");
         return this;
     }
 }
