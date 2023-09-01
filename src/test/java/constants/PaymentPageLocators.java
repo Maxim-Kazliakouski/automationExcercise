e3ac3ae850d8b47da02118f981f96f3fdeac9ce4
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-01 15:49:03 +0300

    feat(): adding PaymentPageLocators

---
+++ src/test/java/constants/PaymentPageLocators.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class PaymentPageLocators {
+    public static final String PAYMENT_PAGE_HEADER = "//h2[text()='Payment']";
+    public static final String PAY_AND_CONFIRM_BUTTON = "submit";
+    public static final String SUCCESSFUL_MESSAGE = "//section//div//p";
+    public static final String DOWNLOAD_INVOICE_BUTTON = "//a[contains(@class, 'check_out')]";
+}
