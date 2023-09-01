e5f82d0b11e991ed6b21c1b79e89d74a537d0d61
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-01 15:53:04 +0300

    feat(): adding logging

--- src/test/java/steps/TestCasesPageSteps.java
+++ src/test/java/steps/TestCasesPageSteps.java
@@ -1,7 +1,9 @@
 package steps;
 
+import lombok.extern.log4j.Log4j2;
 import pages.TestCasesPage;
 
+@Log4j2
 public class TestCasesPageSteps {
     TestCasesPage testCasesPage;
 
@@ -17,5 +19,6 @@ public class TestCasesPageSteps {
      public void isPageOpened() {
         testCasesPage
                 .isOpened();
+        log.info("Test cases page is opened");
      }
 }
