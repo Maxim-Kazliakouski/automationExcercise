6cac136140581fc9708f0ccb0d35d59f560659ba
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-01 15:47:47 +0300

    feat(): adding new method

--- src/test/java/steps/OrderCheckoutSteps.java
+++ src/test/java/steps/OrderCheckoutSteps.java
@@ -1,29 +1,38 @@
 package steps;
 
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
-import dto.Account;
 import dto.OrderCheckout;
-import factories.OrderCheckoutFactory;
-import org.openqa.selenium.By;
-import pages.CartPage;
+import lombok.extern.log4j.Log4j2;
+import pages.BasePage;
 import pages.OrderCheckoutPage;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import static constants.CheckOutPageLocators.PLACE_ORDER_BUTTON;
+import static java.lang.String.format;
 
-import static com.codeborne.selenide.Selenide.*;
-
+@Log4j2
 public class OrderCheckoutSteps {
     OrderCheckoutPage orderCheckoutPage;
+    BasePage basePage;
 
     public OrderCheckoutSteps() {
         orderCheckoutPage = new OrderCheckoutPage();
+        basePage = new BasePage();
     }
 
     public OrderCheckout getData() {
         return
                 orderCheckoutPage
                         .getDataFromCreatedAccount();
+    }
+
+    public OrderCheckoutSteps enterComment(String text) {
+        orderCheckoutPage
+                .enterText(text);
+        log.info(format("Comment '%s' has been entered!", text));
+        return this;
+    }
+
+    public void clickOnPlaceOrderButton() {
+        basePage.clickOnButton(PLACE_ORDER_BUTTON);
+        log.info("Click on 'Place order' button");
     }
 }
