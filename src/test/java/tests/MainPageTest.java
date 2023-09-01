5750c63fd0194ec602da52bb81675610f0dc38bf
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-01 15:47:15 +0300

    feat(): adding new test

--- src/test/java/tests/MainPageTest.java
+++ src/test/java/tests/MainPageTest.java
@@ -4,6 +4,7 @@ import jdk.jfr.Description;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.sleep;
+import static org.testng.Assert.assertEquals;
 
 public class MainPageTest extends BaseTest {
 //    @DataProvider
@@ -39,5 +40,24 @@ public class MainPageTest extends BaseTest {
         mainPageSteps
                 .openPage()
                 .toSubscribe("123@mail.co");
+    }
+
+    @Test
+    @Description("View Category Products")
+    public void viewCategoryProducts() {
+        String categoryName1 = "Men";
+        String subcategoryName1 = "Tshirts";
+        String categoryName2 = "Kids";
+        String subcategoryName2 = "Dress";
+        mainPageSteps
+                .openPage()
+                .isMainPageOpened()
+                .clickOnCategory("Men")
+                .clickOnSubcategory(categoryName1, subcategoryName1);
+        assertEquals(mainPageSteps.checkProductHeader(), (categoryName1 + " - " + subcategoryName1 + " products").toUpperCase());
+        mainPageSteps
+                .clickOnCategory(categoryName2)
+                .clickOnSubcategory(categoryName2, subcategoryName2);
+        assertEquals(mainPageSteps.checkProductHeader(), (categoryName2 + " - " + subcategoryName2 + " products").toUpperCase());
     }
 }
