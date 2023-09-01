4b30cde9862115d6f4b87466dc4a01dd18d1951b
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-01 15:51:34 +0300

    feat(): adding tests

--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -1,16 +1,21 @@
 package tests;
 
 import dto.Account;
-import dto.AccountDataOrderCheckout;
 import dto.OrderCheckout;
+import dto.Payment;
+import dto.ProductReview;
 import factories.AccountFactory;
+import factories.PaymentFactory;
+import factories.ProductReviewFactory;
 import io.qameta.allure.Description;
 import org.testng.annotations.Test;
+import pages.ProductsPage;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
-import static com.codeborne.selenide.Selenide.sleep;
 import static org.testng.Assert.assertEquals;
 
 public class ProductsTest extends BaseTest {
@@ -52,20 +57,21 @@ public class ProductsTest extends BaseTest {
     }
 
     @Test
-    @Description("Test for adding products in cart")
+    @Description("Test for adding products to cart")
     public void addingProductToCart() {
         Map<String, String> testData = Map.ofEntries(
                 Map.entry("product1", "Men Tshirt"),
                 Map.entry("product2", "Stylish Dress"));
         productsPageSteps
                 .openProductsPage()
+                .isProductPageOpened()
                 .addProduct(testData.get("product1"))
                 .clickOnContinueShoppingButton()
                 .addProduct(testData.get("product2"))
                 .clickOnViewCartButton();
         cartPageSteps
                 .isCartPageOpened();
-        assertEquals(cartPageSteps.getAddedProducts(), testData.values());
+        assertEquals(cartPageSteps.getAddedProducts(), new ArrayList<>(testData.values()));
     }
 
     @Test
@@ -119,5 +125,102 @@ public class ProductsTest extends BaseTest {
         cartPageSteps
                 .clickIOnProceedToCheckoutButton();
         assertEquals(orderCheckoutSteps.getData(), dataAccount);
+        signUpPageSteps
+                .deleteAccount();
+    }
+
+    @Test
+    @Description("Place Order: Register before Checkout")
+    public void registerBeforeOrder() {
+        Account account = AccountFactory.getAccount("Mrs", "Canada");
+        Map<String, String> testData = Map.ofEntries(
+                Map.entry("product1", "Men Tshirt"),
+                Map.entry("product2", "Stylish Dress"));
+        Payment payment = PaymentFactory.getPayment();
+        mainPageSteps
+                .isMainPageOpened()
+                .clickOnTab("Signup / Login");
+        signUpPageSteps
+                .isSignUpLoginPageOpened()
+                .createNewUser(account)
+                .isLoggedAsCreatedAccount();
+        productsPageSteps
+                .isProductPageOpened()
+                .addProduct(testData.get("product1"))
+                .clickOnContinueShoppingButton()
+                .addProduct(testData.get("product2"))
+                .clickOnViewCartButton();
+        cartPageSteps
+                .isCartPageOpened()
+                .clickIOnProceedToCheckoutButton();
+        orderCheckoutSteps
+                .enterComment("test")
+                .clickOnPlaceOrderButton();
+        paymentPageSteps
+                .isPaymentPageIsOpened()
+                .createNewPayment(payment);
+        assertEquals(paymentPageSteps.gettingSuccessfulMessage(), "Congratulations! Your order has been confirmed!");
+        signUpPageSteps
+                .deleteAccount();
+    }
+
+    @Test
+    @Description("Place Order: Login before Checkout")
+    public void loginBeforeCheckOut() {
+        Payment payment = PaymentFactory.getPayment();
+        Map<String, String> testData = Map.ofEntries(
+                Map.entry("product1", "Men Tshirt"),
+                Map.entry("product2", "Stylish Dress"));
+        String email = "123@mail.com";
+        String password = "123";
+        String name = "max";
+        mainPageSteps
+                .isMainPageOpened()
+                .clickOnTab("Signup / Login");
+        loginPageSteps
+                .loginWithCreds(email, password);
+        signUpPageSteps
+                .isLoggedAccount(name);
+        productsPageSteps
+                .isProductPageOpened()
+                .addProduct(testData.get("product1"))
+                .clickOnContinueShoppingButton()
+                .addProduct(testData.get("product2"))
+                .clickOnViewCartButton();
+        cartPageSteps
+                .isCartPageOpened()
+                .clickIOnProceedToCheckoutButton();
+        orderCheckoutSteps
+                .enterComment("test")
+                .clickOnPlaceOrderButton();
+        paymentPageSteps
+                .isPaymentPageIsOpened()
+                .createNewPayment(payment);
+        assertEquals(paymentPageSteps.gettingSuccessfulMessage(), "Congratulations! Your order has been confirmed!");
+    }
+
+    @Test
+    @Description("Search Products and Verify Cart After Login")
+    public void searchAndVerifyProducts() {
+        String searchingNameProduct = "Pure Cotton";
+        List<String> expectedProducts = new ArrayList<>(Arrays.asList("Pure Cotton V-Neck T-Shirt", "Pure Cotton Neon Green Tshirt"));
+        productsPageSteps
+                .openProductsPage()
+                .isProductPageOpened()
+                .searchProductByName(searchingNameProduct);
+        assertEquals(ProductsPage.getProductsList(), expectedProducts);
+    }
+
+    @Test
+    @Description("Add review on product")
+    public void addProductReview() {
+        ProductReview productReview = ProductReviewFactory.getReview("Test text for review");
+        productsPageSteps
+                .openProductsPage()
+                .isProductPageOpened()
+                .clickOnViewProductButton("Stylish Dress")
+                .createProductReview(productReview);
+        assertEquals(productsPageSteps.checkSuccessCreateReviewMessage(),
+                "Thank you for your review.", "The product review hasn't been created!");
     }
 }
