c1975f0603f2296eb01bdfc9a14415cb7cb05c61
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-01 15:51:13 +0300

    feat(): adding logging and new methods

--- src/test/java/steps/ProductsPageSteps.java
+++ src/test/java/steps/ProductsPageSteps.java
@@ -1,15 +1,21 @@
 package steps;
 
+import dto.ProductReview;
+import lombok.extern.log4j.Log4j2;
+import pages.BasePage;
 import pages.ProductsPage;
 
-import static com.codeborne.selenide.Selenide.sleep;
+import static constants.ProductsPageLocators.SUBMIT_BUTTON;
 import static java.lang.String.format;
 
+@Log4j2
 public class ProductsPageSteps {
     ProductsPage productsPage;
+    BasePage basePage;
 
     public ProductsPageSteps() {
         productsPage = new ProductsPage();
+        basePage = new BasePage();
     }
 
     public ProductsPageSteps openProductsPage() {
@@ -21,16 +27,19 @@ public class ProductsPageSteps {
     public ProductsPageSteps isProductPageOpened() {
         productsPage
                 .isOpened();
+        log.info("Product page is opened");
         return this;
     }
 
     public ProductsPageSteps getProductsList() {
-        productsPage
+        log.info("Getting product list...");
+        ProductsPage
                 .getProductsList();
         return this;
     }
 
     public void comparingProductsList() {
+        log.info("Comparing products list...");
         productsPage
                 .isProductsListValid();
     }
@@ -38,40 +47,63 @@ public class ProductsPageSteps {
     public void chooseItem(String itemName) {
         productsPage
                 .chooseProductFromTheListByClickingViewButton(itemName);
+        log.info(format("Choose product --> '%s' from the product list", itemName));
     }
 
     public ProductsPageSteps searchProductByName(String productName) {
         productsPage
                 .searchProductByName(productName)
                 .isSearchPageOpened();
+        log.info(format("Searching product --> '%s'", productName));
         return this;
     }
 
     public void areSearchedProductsFound(String productName) {
         productsPage
                 .areSearchedProductsVisible(productName);
+        log.info(format("Searching product --> '%s' has been found", productName));
     }
 
     public ProductsPageSteps addProduct(String productName) {
+        log.info(format("Adding product '%s' to the cart...", productName));
         productsPage
                 .addProduct(productName);
         return this;
     }
 
     public ProductsPageSteps clickOnContinueShoppingButton() {
+        log.info("Click on 'Continue shopping' button");
         productsPage
                 .continuingShoppingFromModalWindowAfterAddingProduct();
         return this;
     }
 
     public ProductsPageSteps clickOnViewCartButton() {
+        log.info("Click on 'View cart' button");
         productsPage
                 .goingToCartFromModalWindowAfterAddingProduct();
+        log.info("Redirecting to the cart page...");
         return this;
     }
 
-    public void clickOnViewProductButton(String itemName) {
+    public ProductsPageSteps clickOnViewProductButton(String itemName) {
         productsPage
                 .chooseProductFromTheListByClickingViewButton(itemName);
+        log.info(format("Choose product '%s' from the list by clicking on view button", itemName));
+        return this;
+    }
+
+    public void createProductReview(ProductReview productReview) {
+        productsPage
+                .createReview(productReview);
+        log.info("Product review has been created");
+        basePage
+                .clickOnButton(SUBMIT_BUTTON);
+        log.info("Click on 'Submit' button");
+    }
+
+    public String checkSuccessCreateReviewMessage() {
+        log.info("Success created review message has been created");
+        return productsPage.gettingSuccessMessage();
     }
 }
