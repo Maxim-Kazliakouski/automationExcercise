48c5645df3a716c1ceba93bb6daebac20c6ce949
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-01 15:43:30 +0300

    feat(): adding new tests for cart page

--- src/test/java/tests/CartPageTest.java
+++ src/test/java/tests/CartPageTest.java
@@ -1,8 +1,23 @@
 package tests;
 
+import dto.Account;
+import dto.OrderCheckout;
+import dto.Payment;
+import factories.AccountFactory;
+import factories.PaymentFactory;
 import jdk.jfr.Description;
+import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.Test;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static java.lang.String.format;
+import static org.testng.Assert.assertEquals;
+
+@Log4j2
 public class CartPageTest extends BaseTest {
 
     @Test
@@ -14,5 +29,90 @@ public class CartPageTest extends BaseTest {
         cartPageSteps
                 .isCartPageOpened()
                 .toSubscribe("111@mail.co");
+    }
+
+    @Test
+    @Description("Remove Products From Cart")
+    public void removeProductsFromCart() {
+        Map<String, String> testData = Map.ofEntries(
+                Map.entry("product1", "Men Tshirt"),
+                Map.entry("product2", "Stylish Dress"));
+        mainPageSteps
+                .openPage()
+                .isMainPageOpened()
+                .clickOnTab("Products");
+        productsPageSteps
+                .isProductPageOpened()
+                .addProduct(testData.get("product1"))
+                .clickOnContinueShoppingButton()
+                .addProduct(testData.get("product2"))
+                .clickOnViewCartButton();
+        cartPageSteps
+                .isCartPageOpened()
+                .deleteItem(testData.get("product1"));
+        assertEquals(cartPageSteps.getAddedProducts().get(0), testData.get("product2"));
+    }
+
+    @Test
+    @Description("Add to cart from Recommended items")
+    public void addRecommendedItems() {
+        List<String> recommendedItems = new ArrayList<>(List.of("Winter Top"));
+        mainPageSteps
+                .openPage()
+                .isMainPageOpened()
+                .scrollToTheRecommendedItemsHeader()
+                .isRecommendedItemsIsVisible()
+                .chooseRecommendedItem(recommendedItems.get(0));
+        productsPageSteps
+                .clickOnViewCartButton();
+        cartPageSteps
+                .isCartPageOpened();
+        assertEquals(cartPageSteps.getAddedProducts(), recommendedItems, "The recommended items in the cart doesn't match!");
+    }
+
+    @Test
+    @Description("Download Invoice after purchase order")
+    public void downloadInvoiceAfterPurchaseOrder() throws FileNotFoundException {
+        Account account = AccountFactory.getAccount("Mrs", "Canada");
+        OrderCheckout dataAccount = AccountFactory.getDataAccountForOrderCheckoutPage("Mrs", "Canada");
+        Payment payment = PaymentFactory.getPayment();
+        String expectedInvoiceData = format("Hi %s %s, Your total purchase amount is 1900. Thank you", dataAccount.getFirstName(), dataAccount.getLastName());
+        Map<String, String> testData = Map.ofEntries(
+                Map.entry("product1", "Men Tshirt"),
+                Map.entry("product2", "Stylish Dress"));
+        mainPageSteps
+                .openPage()
+                .isMainPageOpened();
+        productsPageSteps
+                .addProduct(testData.get("product1"))
+                .clickOnContinueShoppingButton()
+                .addProduct(testData.get("product2"))
+                .clickOnViewCartButton();
+        cartPageSteps
+                .isCartPageOpened()
+                .clickIOnProceedToCheckoutButton()
+                .clickOnRegisterLoginButton();
+        signUpPageSteps
+                .createNewUser(account)
+                .isAccountCreated()
+                .isLoggedAsCreatedAccount();
+        mainPageSteps
+                .clickOnTab("Cart");
+        cartPageSteps
+                .clickIOnProceedToCheckoutButton();
+        assertEquals(orderCheckoutSteps.getData(), dataAccount);
+        orderCheckoutSteps
+                .enterComment("Test")
+                .clickOnPlaceOrderButton();
+        paymentPageSteps
+                .isPaymentPageIsOpened()
+                .createNewPayment(payment)
+                .gettingSuccessfulMessage();
+        assertEquals(paymentPageSteps.gettingInvoice(), expectedInvoiceData,
+                "The data in voice doesn't match with expected data");
+        paymentPageSteps
+                .clickOnContinueButton();
+        signUpPageSteps
+                .deleteAccount();
     }
 }
