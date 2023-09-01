9e01cf8a963f1ca91f338d409e54c79221b8e7f8
max_kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-01 15:50:06 +0300

    feat(): adding logging

--- src/test/java/steps/ProductsDetailsPageSteps.java
+++ src/test/java/steps/ProductsDetailsPageSteps.java
@@ -1,7 +1,11 @@
 package steps;
 
+import lombok.extern.log4j.Log4j2;
 import pages.ProductDetailsPage;
 
+import static java.lang.String.format;
+
+@Log4j2
 public class ProductsDetailsPageSteps {
     ProductDetailsPage productDetailsPage;
 
@@ -12,23 +16,27 @@ public class ProductsDetailsPageSteps {
     public ProductsDetailsPageSteps isProductDetailsPageOpened() {
         productDetailsPage
                 .isOpened();
+        log.info("Product detail page is opened");
         return this;
     }
 
     public ProductsDetailsPageSteps increaseProductQuantity(Integer quantity) {
         productDetailsPage
                 .increaseQuantity(quantity);
+        log.info(format("The product quantity has been increased to --> %s", quantity));
         return this;
     }
 
     public ProductsDetailsPageSteps addToCart() {
         productDetailsPage
                 .clickOnAddToCartButton();
+        log.info("Click on add to cart button");
         return this;
     }
 
     public void viewCart() {
         productDetailsPage
                 .clickOnViewCartButton();
+        log.info("Click on view cart button");
     }
 }
