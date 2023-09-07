75d0d2e55f873c1214c71e4724a3a2b86f623aed
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-07 16:57:12 +0300

    feat(): adding TestRunRunnerChecker for check already created runs

---
+++ src/test/java/utils/TestRunsChecker.java
@@ -0,0 +1,34 @@
+package utils;
+
+import adapters.ProjectAPI;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import dto.QaseProject.APIResponse;
+import dto.QaseProject.Project;
+import io.restassured.response.Response;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class TestRunsChecker {
+    public void isTestRunCreated() {
+        boolean isTestRun = Boolean.parseBoolean(PropertyReader.getProperty("testRun"));
+        boolean isAPI = Boolean.parseBoolean(PropertyReader.getProperty("api"));
+        if (!isAPI && isTestRun) {
+            Gson gson = new Gson();
+            ProjectAPI projectAPI = new ProjectAPI();
+            Response response = projectAPI.getAllRuns(PropertyReader.getProperty("codeProject"));
+            APIResponse<Project> result = gson.fromJson(response.asString(),
+                    new TypeToken<APIResponse<Project>>() {
+                    }.getType());
+            int count = result.getResult().getCount();
+            if (count != 0) {
+                int testRunID = result.getResult().entities.get(0).getId();
+                projectAPI.deleteTestRun(PropertyReader.getProperty("codeProject"), testRunID);
+                System.out.println("<----> Test run has been deleted! <---->");
+            } else {
+                System.out.println("There is no created test runs");
+                log.info("<----> Test run hasn't been created yet! <---->");
+            }
+        }
+    }
+}
