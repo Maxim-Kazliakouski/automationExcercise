05107ac642ca6722bdf0579f6ee08d6a9095df99
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-07 16:58:07 +0300

    feat(): adding tmslink

--- src/test/java/tests/ContactUsTest.java
+++ src/test/java/tests/ContactUsTest.java
@@ -1,9 +1,11 @@
 package tests;
 
 import io.qameta.allure.Description;
+import io.qameta.allure.TmsLink;
 import org.testng.annotations.Test;
 
 public class ContactUsTest extends BaseTest{
+    @TmsLink("case=1")
     @Test
     @Description("Test for checking that user can contact with support")
     public void sendEmailForSupport() {
