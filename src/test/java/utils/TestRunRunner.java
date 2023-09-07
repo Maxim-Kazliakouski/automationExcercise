9d6d9b676faa75d5490973e09b3adc56f1f60c6a
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-07 16:56:48 +0300

    feat(): adding TestRunRunner for creating runner in Qase

---
+++ src/test/java/utils/TestRunRunner.java
@@ -0,0 +1,19 @@
+package utils;
+
+import adapters.ProjectAPI;
+import io.restassured.response.Response;
+
+public class TestRunRunner {
+    ProjectAPI projectAPI;
+    public Integer launchTestRun(String testRunName) {
+        String startTestRun = PropertyReader.getProperty("testRun");
+        String codeProject = PropertyReader.getProperty("codeProject");
+        if (Boolean.parseBoolean(startTestRun)) {
+            System.out.printf("======================================== Creating test run for project --> %s ========================================%n", PropertyReader.getProperty("codeProject"));
+            projectAPI = new ProjectAPI();
+            Response response = projectAPI.createTestRun(codeProject, testRunName);
+            return response.path("result.id");
+        }
+        else return null;
+    }
+}
