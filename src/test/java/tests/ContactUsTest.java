6a878ca75a2b725c741dda4413a33c0c10b7b865
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-13 16:37:29 +0300

    feat(): update

--- src/test/java/tests/ContactUsTest.java
+++ src/test/java/tests/ContactUsTest.java
@@ -2,15 +2,14 @@ package tests;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.TmsLink;
-import org.junit.jupiter.api.Test;
-import org.testng.ITestResult;
+import org.testng.annotations.Test;
 
 public class ContactUsTest extends BaseTest {
     @TmsLink("case=1")
     @Test
     @Description("Test for checking that user can contact with support")
-    public void sendEmailForSupport(ITestResult info) {
-        assertScreenshots(info);
+    public void sendEmailForSupport() {
+        assertScreenshots(getTestCaseName());
         contactUsPageSteps
                 .openPage()
                 .createNewEmailBody(chooseOS())
