90a47f5bad759f920329d744ffe33f0a0c81b5af
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-13 15:51:40 +0300

    feat(): adding method in test for comparison images

--- src/test/java/tests/ContactUsTest.java
+++ src/test/java/tests/ContactUsTest.java
@@ -2,13 +2,15 @@ package tests;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.TmsLink;
+import org.junit.jupiter.api.TestInfo;
 import org.testng.annotations.Test;
 
 public class ContactUsTest extends BaseTest {
     @TmsLink("case=1")
     @Test
     @Description("Test for checking that user can contact with support")
-    public void sendEmailForSupport() {
+    public void sendEmailForSupport(TestInfo info) {
+        assertScreenshots(info);
         contactUsPageSteps
                 .openPage()
                 .createNewEmailBody(chooseOS())
