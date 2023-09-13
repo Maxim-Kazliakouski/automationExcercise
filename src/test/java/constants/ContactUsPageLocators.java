ee76db1bf0e5a711629b3e4cbe1205459624c8d1
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-09-13 18:19:25 +0300

    feat(): update

--- src/test/java/constants/ContactUsPageLocators.java
+++ src/test/java/constants/ContactUsPageLocators.java
@@ -3,5 +3,5 @@ package constants;
 public class ContactUsPageLocators {
     public static final String CONTACT_US_SUBMIT_BUTTON = "//input[@data-qa='submit-button']";
     public static final String SUCCESS_NOTIFICATION = "//div[text()='Success! Your details have been submitted successfully.']";
-    public static final String HOME_BUTTON = ".btn-success";
+    public static final String HOME_BUTTON = ".btn-success1";
 }
