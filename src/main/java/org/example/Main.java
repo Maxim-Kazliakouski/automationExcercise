dc5b7e6eec56a01ad33e2cfae2316174d36c3b65
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2023-10-19 14:05:30 +0300

    feat(): fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String text = "Max Kazliakosuki is QA Automation Engineer";
+        Pattern pattern = Pattern.compile("i.++n");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println(text.substring(matcher.start(), matcher.end()));
+        }
     }
 }
